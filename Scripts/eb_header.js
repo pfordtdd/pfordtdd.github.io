c1f4305944b514d3588af381fc3131afafd47265
pfordtdd <154999204+pfordtdd@users.noreply.github.com>
2026-07-28 11:22:52 +0100

    Update eb_header.js

--- Scripts/eb_header.js
+++ Scripts/eb_header.js
@@ -2,5 +2,5 @@
 /* Note: The following script inserts the LDGCBR Logo and Bell picture in the header of the Eastern Branch pages */
 
 document.write("<div class='topContainer'>");
-document.write("<img src='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/img_bin/ldg_logo.jpg' height='146'><img src='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/img_bin/header.jpg' height='146' align='right'>");
+document.write("<img src='https://pfordtdd.github.io/img_bin/ldg_logo.jpg' height='146'><img src='https://pfordtdd.github.io/img_bin/header.jpg' height='146' align='right'>");
 document.write("</div>");
