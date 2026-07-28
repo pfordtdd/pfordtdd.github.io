e082584aa0b90f11db533a84c217673ecd9cc379
pfordtdd <154999204+pfordtdd@users.noreply.github.com>
2026-07-28 11:18:58 +0100

    Update eb_menu.js

--- Scripts/eb_menu.js
+++ Scripts/eb_menu.js
@@ -5,7 +5,7 @@ document.write("<div class='menuContainer' align='center'>");
 document.write("<table><tr>");
 document.write("<td><a href='http://www.ldgcb.com/'>LDGCBR</a></td>");
 document.write("<td><a href='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/index.html'>HOME</a></td>");
-document.write("<td><a href='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/Pages/News/News_2025/News_2025.html'>NEWS</a></td>");
+document.write("<td><a href='https://pfordtdd.github.io/Pages/News/News_2025/News_2025.html'>NEWS</a></td>");
 document.write("<td><a href='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/Pages/Events/Events.html'>EVENTS</a></td>");
 document.write("<td><a href='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/Pages/Quarter_Peals/QP_2026.html'>QUARTER PEALS</a></td>");
 document.write("<td><a href='/home/philf/Data/Bell Ringers/Eastern Branch Website/Eastern/eastern/Pages/People/Officers.html'>PEOPLE</a></td>");
